e9567a84080debb5dd8781427f34ad928b69bac8
jacksendall <jacksendall@gmail.com>
2020-03-19 15:16:44 +0000

    new resources

--- js/slider.js
+++ js/slider.js
@@ -4,11 +4,12 @@
 //     console.log(new Date());
 // }, 1000)
 
-// Slide to the right
+// Slide one to the right
 // $('#slider .slides').animate({ 'margin-left': '-=100vw' }, 1000);
 
-// Slide to the left
+// Slide one to the left
 //$('#slider .slides').animate({ 'margin-left': '+=100vw' }, 1000);
+
 $(function() {
 
     //Cache DOM
@@ -16,6 +17,8 @@ $(function() {
     var $slideContainer = $slider.find('.slides');
     var $slides = $slideContainer.find('.slide');
     var currentSlide = 1;
+    var $sliderControls = $('.slider-controls');
+    var $slideButtons = $sliderControls.find('.dot');
 
     console.log($slides);
 
@@ -25,21 +28,19 @@ $(function() {
     function startSlider() {
         myInterval = setInterval(function() {
             //animate margin-left
-            $slideContainer.animate({ 'margin-left': '-=100vw' }, 200, function() {
+            $slideContainer.animate({ 'margin-left': '-=100vw' }, 400, function() {
                 currentSlide++;
                 if (currentSlide === $slides.length) {
                     currentSlide = 1;
                     $slideContainer.css('margin-left', 0);
                 };
             });
-        }, 5000)
+        }, 4500)
     }
 
     function stopSlider() {
         clearInterval(myInterval);
     }
-
-
 
     //listen for mouseover and pause
     $slider.on('mouseenter', stopSlider).on('mouseleave', startSlider);

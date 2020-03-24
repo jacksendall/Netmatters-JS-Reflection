0ad81676191017637c716df88f9a1e115ff17f88
jacksendall <jacksendall@gmail.com>
2020-03-24 17:44:18 +0000

    Added Sidebar
    +added sidebar
    +added sidebar css
    +added sidebar js/jQuery

--- js/mmenu/init.js
+++ js/mmenu/init.js
@@ -2,14 +2,31 @@
 //     $('#sidebar').mmenu({ "extensions": ["position-back", "position-right"] });
 //     console.log("sidebar initialized.");
 // });
-const menu = new Mmenu("#sidebar", { "extensions": ["position-back", "position-right"] });
-var api = menu.API;
-console.log(api);
-
+const menu = new Mmenu("#sidebar", {
+    "extensions": ["position-back", "position-right"]
+});
 
 
 // Get the API
-// var api = menu.API;
+var api = menu.API;
+//log api
+console.log(api);
+
+
+api.bind("closeMenu",
+    () => {
+        $(".content-wrapper").toggleClass("blackout");
+        console.log("close");
+    }
+);
+api.bind("openMenu",
+    () => {
+        $(".content-wrapper").toggleClass("blackout");
+        console.log("open");
+    }
+);
+
+
 
 
 
@@ -17,4 +34,12 @@ console.log(api);
 $('.btn-menu-nm').click(function() {
     api.open();
     console.log("sidebar opened.");
+    $(".content-wrapper").toggleClass("blackout");
+
+    $('#container').click(function() {
+        $(".content-wrapper").toggleClass("blackout");
+    });
+
+    $(".content-wrapper").toggleClass("blackout");
+
 });
--- js/sidebar.js
+++ js/sidebar.js
@@ -1,23 +1,3 @@
-var menuButton = document.querySelector('.btn-menu-nm');
-let menuState = false;
-
-//On click change menu state
-menuButton.addEventListener('click', () =>{
-    if(menuState === false){
-        //show menu
-
-        menuState = true;
-    }else {
-        //hide menu
-
-
-        menuState = false;
-        
-    }
-});
-
-setInterval(function(){
-    //DEBUG
-    console.log(menuState);
-
-}, 1000);
+function toggleSidebar() {
+    $('body').toggleClass('menu-active')
+};
